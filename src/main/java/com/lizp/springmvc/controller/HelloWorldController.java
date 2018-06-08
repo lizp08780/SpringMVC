9ac4a836728af4f905406b37f850824e35454752
lizhipeng@bwton.com <lizhipeng@bwton.com>
2018-06-08 16:43:38 +0800

    del

--- src/main/java/com/lizp/springmvc/controller/HelloWorldController.java
+++ src/main/java/com/lizp/springmvc/controller/HelloWorldController.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 import com.lizp.springmvc.domain.Person;
 import com.lizp.springmvc.service.ITxChannelService;
@@ -20,8 +19,6 @@ public class HelloWorldController {
 	private Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
 	@Autowired
 	private ITxChannelService txChannelService;
-	@Autowired
-	private RequestMappingHandlerAdapter requestMappingHandlerAdapter;
 
 	@RequestMapping("/hello")
 	public String hello(Model model) {
@@ -39,7 +36,6 @@ public class HelloWorldController {
 	@RequestMapping("/json")
 	@ResponseBody
 	public Person json() {
-		requestMappingHandlerAdapter.getMessageConverters();
 		logger.info("=========json==============");
 		// int x = 1 / 0;跳转500界面
 		Person person = new Person();
