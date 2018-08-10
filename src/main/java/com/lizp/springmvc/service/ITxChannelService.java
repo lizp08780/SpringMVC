5366aa7c045520810fd4a75a6a50f65cc4d3bd6e
lizhipeng@bwton.com <lizhipeng@bwton.com>
2018-08-10 23:45:29 +0800

    注释

--- src/main/java/com/lizp/springmvc/service/ITxChannelService.java
+++ src/main/java/com/lizp/springmvc/service/ITxChannelService.java
@@ -3,12 +3,30 @@ package com.lizp.springmvc.service;
 import com.lizp.springmvc.domain.TxChannel;
 
 public interface ITxChannelService {
+	/**
+	 * @param channelSerialNo
+	 * @param custId
+	 * @return
+	 */
 	TxChannel selectById(String channelSerialNo, String custId);
 
+	/**
+	 * @param paySerialNo
+	 * @param custId
+	 * @return
+	 */
 	TxChannel selectByPaySerialNo(String paySerialNo, String custId);
 
+	/**
+	 * @param txChannel
+	 * @return
+	 */
 	int insertSelective(TxChannel txChannel);
 
+	/**
+	 * @param txChannel
+	 * @return
+	 */
 	int updateById(TxChannel txChannel);
 
 }
