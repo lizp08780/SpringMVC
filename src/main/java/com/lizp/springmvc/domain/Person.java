5ceac26d3491496f2c6f108f95f62788ffa56d71
lizhipeng@bwton.com <lizhipeng@bwton.com>
2018-08-12 13:47:01 +0800

    编码错误

--- src/main/java/com/lizp/springmvc/domain/Person.java
+++ src/main/java/com/lizp/springmvc/domain/Person.java
@@ -1,45 +1,48 @@
-package com.lizp.springmvc.domain;
+﻿package com.lizp.springmvc.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.google.common.base.Objects;
 
 import java.util.Date;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class Person {
-	private int age;
-	private String name;
-	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
-	private Date create;
+    private int age;
+    private String name;
 
-	public int getAge() {
-		return age;
-	}
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    private Date create;
 
-	public void setAge(int age) {
-		this.age = age;
-	}
+    public int getAge() {
+        return age;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setAge(int age) {
+        this.age = age;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public Date getCreate() {
-		return create;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setCreate(Date create) {
-		this.create = create;
-	}
+    public Date getCreate() {
+        return create;
+    }
 
-	@Override
-	public String toString() {
-		return "Person{" +
-				"age=" + age +
-				", name='" + name + '\'' +
-				", create=" + create +
-				'}';
-	}
+    public void setCreate(Date create) {
+        this.create = create;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", create=" + create +
+                '}';
+    }
 }
